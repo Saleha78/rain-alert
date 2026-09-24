23ea4a51ee356a96f68bed92b16ccd5e3d6d306d
Saleha Sanober <salehamallick1622@gmail.com>
2026-09-24 18:43:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,73 @@
 import os
+import smtplib
 import requests
+from email.message import EmailMessage
 from twilio.rest import Client
+
+SENDERS_EMAIL = os.environ.get("SENDERS_EMAIL")
+RECEIVERS_EMAIL = os.environ.get("RECEIVERS_EMAIL")
+PASSWORD = os.environ.get("EMAIL_PASSWORD")
 
 api_key = os.environ.get("OWM_API_KEY")
 account_sid = os.environ.get("ACCOUNT_SID")
 auth_token = os.environ.get("AUTH_TOKEN")
 
-weather_params = {
-    "lat": 23.642139 ,
+client = Client(account_sid, auth_token)
+
+OWM_endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+
+parameter = {
+    "lat": 23.642139,
     "lon": 86.171681,
     "appid": api_key,
-    "cnt": 4,
+    "cnt": 4
 }
 
-response = requests.get(
-    "https://api.openweathermap.org/data/2.5/forecast",
-    params=weather_params
-)
-
+response = requests.get(OWM_endpoint, params=parameter)
 response.raise_for_status()
 weather_data = response.json()
+
+rain_data = weather_data["list"][0]["weather"][0]["id"]
+print(rain_data)
 
 will_rain = False
 
 for hour_data in weather_data["list"]:
-    weather_id = hour_data["weather"][0]["id"]
+    condition_code = hour_data["weather"][0]["id"]
 
-    if weather_id < 700:
+    if condition_code < 700:
         will_rain = True
 
 if will_rain:
-    client = Client(account_sid, auth_token)
+    try:
+        message = client.messages.create(
+            to="+918540928409",
+            from_="+17372508034",
+            body="It's going to rain today. Remember to bring an umbrella ☔",
+        )
 
-    message = client.messages.create(
-        body="It's going to rain today. Remember to bring an umbrella ☔",
-        from_="+17372508034",
-        to="+918540928409",
-    )
+        print(message.status)
 
-    print(message.status)
+    except:
+        connection = smtplib.SMTP("smtp.gmail.com", 587)
+        connection.starttls()
+
+        connection.login(
+            user=SENDERS_EMAIL,
+            password=PASSWORD
+        )
+
+        email = EmailMessage()
+        email["Subject"] = "Rain Alert..☔ Carry an Umbrella"
+        email["From"] = SENDERS_EMAIL
+        email["To"] = RECEIVERS_EMAIL
+
+        email.set_content(
+            "It's going to rain today. Remember to carry an umbrella. "
+            "Take care and don't get sick! ❤️"
+        )
+
+        connection.send_message(email)
+        connection.close()
+
+        print("Email sent!")
